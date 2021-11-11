5ccf87e6bd44017a2d2f8803e07832d84b297a83
Cassiana <lima.cglima@gmail.com>
2021-11-11 12:19:17 -0300

    duas novas regras e novos testes e testando exceções e TDD

--- src/leilao/test_leilao.py
+++ src/leilao/test_leilao.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from src.leilao.dominio import Usuario, Lance, Leilao
 
 
-class TestAvaliador(TestCase):
+class TestLeilao(TestCase):
 
     # test_quando_adicionados_em_ordem_crescente_deve_retornar_o_maior_e_o_menor_valor_de_um_lance
 
@@ -42,8 +42,8 @@ class TestAvaliador(TestCase):
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
 
-        self.leilao.lances.append(self.lance_do_gui)
-
+        self.leilao.propoe(self.lance_do_gui)
+        self.leilao.propoe(lance_do_yuri)
         menor_valor_esperado = 150.0
         maior_valor_esperado = 150.0
 
@@ -65,3 +65,23 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+    def test_deve_permitir_propor_um_lance_caso_o_leilao_nao_tenha_lance(self):
+        self.leilao.propoe(self.lance_do_gui)
+        qunatidade_de_lances_recebido = len(self.leilao.lances)
+        self.assertEqual(1, qunatidade_de_lances_recebido)
+
+    def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_seja_diferente(self):
+        yuri = Usuario('Yuri')
+        lance_do_yuri = Lance(yuri, 200.0)
+        self.leilao.propoe(self.lance_do_gui)
+        self.leilao.propoe(lance_do_yuri)
+        qunatidade_de_lances_recebido = len(self.leilao.lances)
+        self.assertEqual(2, qunatidade_de_lances_recebido)
+
+    def test_nao_deve_permitir_propor_lance_caso_o_usuario_seja_o_mesmo(self):
+        lance_do_gui = Lance(self.gui, 200.0)
+
+        with self.assertRaises(ValueError):
+            self.leilao.propoe(self.lance_do_gui)
+            self.leilao.propoe(lance_do_gui)
